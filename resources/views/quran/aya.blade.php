9393b18ea1e53fae648792c63e1e554273d554a0
dwijpr <dwijpr@gmail.com>
2017-07-29 17:42:04 +0700

    aya number inline with text

--- resources/views/quran/aya.blade.php
+++ resources/views/quran/aya.blade.php
@@ -4,12 +4,12 @@
     lang="ar"
 >
     {!! clean($aya->text) !!}
-</div>
-<div
-    class="aya-number"
->
-    {{ Html::image('/img/aya-number-frame.png') }}
-    <div class="number text-center">{{ ar($aya->aya_id) }}</div>
+    <div
+        class="aya-number"
+    >
+        {{ Html::image('/img/aya-number-frame.png') }}
+        <div class="number text-center">{{ ar($aya->aya_id) }}</div>
+    </div>
 </div>
 <hr>
 <div>{{ $aya->terjemahan }}</div>
